9721817675cf76899d102ef12de3468e6976b4cf
Duy Nguyen <tanduy.nguyen@2359media.com>
2016-12-02 14:44:31 +0700

    Handle problem when remove timeAssociation

--- ios-ntp-lib/NetworkClock.m
+++ ios-ntp-lib/NetworkClock.m
@@ -90,9 +90,11 @@
             }
             else {
                 if (timeAssociations.count > 8) {
-                NSLog(@"Clock•Drop: [%@]", timeAssociation.server);
-                    [timeAssociations removeObject:timeAssociation];
-                    [timeAssociation finish];
+//                NSLog(@"Clock•Drop: [%@]", timeAssociation.server);
+                    if ([timeAssociations containsObject:timeAssociation]) {
+                        [timeAssociations removeObject:timeAssociation];
+                        [timeAssociation finish];
+                    }
                 }
             }
 
